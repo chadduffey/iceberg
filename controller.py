981a23c1391414e70293dacaee15b283a34a018b
Chad Duffey <chadduffey@hotmail.com>
2017-09-13 15:05:42 -0700

    add iceberg queue

--- controller.py
+++ controller.py
@@ -20,15 +20,53 @@ systems_list_name = base_path + "\\service\\systems.txt"
 
 #----------------------------------------
 
+
+def review_iceberg_systems_file():
+	iceberg_systems = []
+	with open(systems_list_name, "r") as f:
+		for system in f:
+			system = system.rstrip()
+			iceberg_systems.append(system)
+	return iceberg_systems
+
+
+def database_setup(systems):
+	print("(Database Setup): Checking...")
+	with sqlite3.connect(database_location) as connection:
+		c = connection.cursor()
+		c.execute("CREATE TABLE IF NOT EXISTS Systems(SystemName TEXT UNIQUE)")
+
+		for system in systems:
+			c.execute("INSERT OR IGNORE INTO Systems VALUES('" + system + "')")
+
+		print("(Database Setup): Looking good.")
+
+def iceberg_systems_from_db():
+	all_iceberg_systems = []
+	with sqlite3.connect(database_location) as connection:
+		c = connection.cursor()
+		c.execute("SELECT SystemName FROM Systems")
+		for row in c.fetchall():
+			all_iceberg_systems.append(row[0])
+	return all_iceberg_systems
+
+def iceberg_queue_smasher(queue):
+	print("(Processing the queue): Smasher Iteration")
+	for _ in range(len(queue)):
+		print(queue[_].name)
+
+#CHECK PATHS AND LOCATIONS MAKE SENSE
 iceberg.Iceberg.health_check(systems_list_name, base_path, database_location)
 
-with sqlite3.connect(database_location) as connection:
-	c = connection.cursor()
+#SETUP DATABASE
+database_setup(review_iceberg_systems_file())
 
-	c.execute("CREATE TABLE IF NOT EXISTS Systems(SystemName TEXT UNIQUE)")
+#BUILD ICEBERG OBJECTS FOR EACH SYSTEM AND ADD THEM TO THE QUEUE
+controller_queue = []
+for system in iceberg_systems_from_db():
+	controller_queue.append(iceberg.Iceberg(name=system))
 
-	c.execute("INSERT OR IGNORE INTO Systems VALUES('Salesforce')")
-
-
-system1 = iceberg.Iceberg(name="system1")
-print(system1)
+#MAIN LOOP
+#while greater than zero in the queue loop will go here.
+print("\n(Processing the Queue): Starting")
+iceberg_queue_smasher(controller_queue)
