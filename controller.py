5af24c6aaca51cc96fdf3c5f7d0d2105aaa351c8
Chad Duffey <chad@dropbox.internal>
2017-09-12 18:05:31 -0700

    basic db connection

--- controller.py
+++ controller.py
@@ -9,9 +9,23 @@
 #clean up
 
 import iceberg
+import sqlite3
+
+#----------------------------------------
+# modify:
+
+database_name = "iceberg.db"
+database_location = "c:\\iceberg\\database\\"
+
+#----------------------------------------
+
+with sqlite3.connect(database_location + database_name) as connection:
+	c = connection.cursor()
+
+	c.execute("CREATE TABLE IF NOT EXISTS Systems(SystemName TEXT UNIQUE)")
+
+	c.execute("INSERT OR IGNORE INTO Systems VALUES('Salesforce')")
+
 
 system1 = iceberg.Iceberg(name="system1")
-
 print(system1)
-
-
